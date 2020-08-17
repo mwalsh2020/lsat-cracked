59f72e9b255b393ce921a0d656cc9de786216dbd
Caio Andrade <1820441+caioertai@users.noreply.github.com>
2020-08-16 22:53:12 -0500

    Move course policy to Course::Dashboard

--- app/controllers/course/dashboards_controller.rb
+++ app/controllers/course/dashboards_controller.rb
@@ -1,6 +1,6 @@
 class Course::DashboardsController < ApplicationController
   def show
-    authorize :course
+    authorize [:course, :dashboard]
     @section = Section.first
     redirect_to course_section_path(@section)
   end
--- app/policies/course/dashboard_policy.rb
+++ app/policies/course/dashboard_policy.rb
@@ -1,4 +1,4 @@
-class CoursePolicy < ApplicationPolicy
+class Course::DashboardPolicy < ApplicationPolicy
   def show?
     true
   end
