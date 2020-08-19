b640668e7839c336f44ccfe5f419b48d6fd85e96
Caio Andrade <1820441+caioertai@users.noreply.github.com>
2020-08-18 19:25:59 -0500

    Config generators

--- config/application.rb
+++ config/application.rb
@@ -8,13 +8,18 @@ Bundler.require(*Rails.groups)
 
 module LsatCracked
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    config.generators do |generate|
+      generate.helper false
+      generate.javascripts false
+      generate.request_specs false
+      generate.routing_specs false
+      generate.stylesheets false
+      generate.test_framework :rspec
+      generate.view_specs false
+    end
+
     config.to_prepare do
       ActionText::ContentHelper.allowed_tags << "iframe"
     end
