c2e26440b282f6f3bf84f7991300af10adf184a5
Caio Andrade <1820441+caioertai@users.noreply.github.com>
2020-08-18 19:41:08 -0500

    Add User#answered_quiz?(section)

--- app/models/quiz_session.rb
+++ app/models/quiz_session.rb
@@ -2,7 +2,7 @@ class QuizSession < ApplicationRecord
   belongs_to :user
   belongs_to :section
 
-  has_many :quiz_questions
+  has_many :quiz_questions, dependent: :destroy
   has_many :questions, through: :quiz_questions
 
   accepts_nested_attributes_for :quiz_questions
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :recoverable, :rememberable, :validatable
+
+  def answered_quiz?(section)
+    QuizSession.complete.exists?(user: self, section: section)
+  end
 end
