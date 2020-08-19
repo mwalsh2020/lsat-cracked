b39d64dc9a49dc5ab87e1adf6b3c798a8fa35e9e
Caio Andrade <1820441+caioertai@users.noreply.github.com>
2020-08-19 13:56:10 -0500

    Add optional to quiz question answer

--- app/controllers/course/sections_controller.rb
+++ app/controllers/course/sections_controller.rb
@@ -11,7 +11,7 @@ class Course::SectionsController < Course::ApplicationController
   def build_or_find_quiz_session
     # TODO: move user filtering to a policy scope
     quiz_session = policy_scope(QuizSession).pending.find_or_initialize_by(section: @section)
-    quiz_session.update(questions: @section.questions) unless quiz_session.persisted?
+    quiz_session.update!(questions: @section.questions) unless quiz_session.persisted?
     quiz_session
   end
 end
--- app/models/quiz_question.rb
+++ app/models/quiz_question.rb
@@ -1,7 +1,7 @@
 class QuizQuestion < ApplicationRecord
   belongs_to :quiz_session
   belongs_to :question
-  belongs_to :answer
+  belongs_to :answer, optional: true
 
   has_many :possible_answers, through: :question, source: :answers
 
