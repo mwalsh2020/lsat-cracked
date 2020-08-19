6983d13c2a41769332d99ab03d14ea2841c60449
Caio Andrade <1820441+caioertai@users.noreply.github.com>
2020-08-18 19:18:37 -0500

    Remove test folder

--- test/controllers/admin/answers_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class Admin::AnswersControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get admin_answers_new_url
-    assert_response :success
-  end
-
-end
