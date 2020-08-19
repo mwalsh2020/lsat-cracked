b9aecef96291526a973b076d08bf976de19659d1
Caio Andrade <1820441+caioertai@users.noreply.github.com>
2020-08-18 20:18:01 -0500

    Add User#pending_quiz(section)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,13 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :recoverable, :rememberable, :validatable
 
+  has_many :quiz_sessions, dependent: :destroy
+
+  def pending_quiz(section)
+    quiz_sessions.pending.find_by(user: self, section: section)
+  end
+
   def answered_quiz?(section)
-    QuizSession.complete.exists?(user: self, section: section)
+    quiz_sessions.complete.exists?(user: self, section: section)
   end
 end
