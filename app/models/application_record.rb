a6e1c8f778bca2baa460a72ea19908ba0af8364b
Caio Andrade <1820441+caioertai@users.noreply.github.com>
2020-08-25 10:31:13 -0500

    Add null User methods to User::Guest

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,9 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def registerable?
+    return false unless self.class.methods.include?(:devise_modules)
+
+    self.class.devise_modules.include?(:registrations)
+  end
 end
--- app/models/user/guest.rb
+++ app/models/user/guest.rb
@@ -1,2 +1,17 @@
 class User::Guest
+  def admin?
+    false
+  end
+
+  def present?
+    false
+  end
+
+  def guest?
+    true
+  end
+
+  def registerable?
+    User.new.registerable?
+  end
 end
