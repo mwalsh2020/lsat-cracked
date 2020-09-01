1a6232d58b3169af756bc8e57ba96d72d936dfca
Caio Andrade <1820441+caioertai@users.noreply.github.com>
2020-09-01 12:44:33 -0500

    Add routes to products

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   root                  to: "pages#home"
   get  "/about",        to: "pages#about"
   get  "/testimonials", to: "pages#testimonials"
+
   resources :coaching_requests, only: [:new, :create]
+  resources :products, only: [:index]
 
   namespace :admin do
     root to: "dashboards#show"
