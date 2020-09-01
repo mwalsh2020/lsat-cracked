84b32fffb172894430d8b09df109559fe210feeb
Caio Andrade <1820441+caioertai@users.noreply.github.com>
2020-09-01 13:04:33 -0500

    Add full_price to products

--- db/migrate/20200901174302_create_products.rb
+++ db/migrate/20200901174302_create_products.rb
@@ -1,9 +1,10 @@
-class CreatePackages < ActiveRecord::Migration[6.0]
+class CreateProducts < ActiveRecord::Migration[6.0]
   def change
-    create_table :packages do |t|
+    create_table :products do |t|
       t.string :name
       t.string :sku
       t.monetize :price, currency: { present: false }
+      t.monetize :full_price, currency: { present: false }
 
       t.timestamps
     end
--- db/migrate/20200901174838_rename_packages_to_products.rb
+++
@@ -1,5 +0,0 @@
-class RenamePackagesToProducts < ActiveRecord::Migration[6.0]
-  def change
-    rename_table :packages, :products
-  end
-end
