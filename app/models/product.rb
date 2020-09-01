8e326a07c3890bee0f25a2003f9c245cb5b6a6e1
Caio Andrade <1820441+caioertai@users.noreply.github.com>
2020-09-01 13:09:54 -0500

    Add products to seeds

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ApplicationRecord
+  validates :sku, uniqueness: true
+  validates :name, :sku, :price, :full_price, presence: true
+
+  monetize :price_cents
+  monetize :full_price_cents
+
+  has_rich_text :description
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,29 @@ User.create!(user_params)
 
 puts "Importing book content..."
 Rake::Task["db:seed:course_content"].invoke
+
+puts "Creating products..."
+
+Product.create!(
+  name: "Package 1",
+  sku: "package-1",
+  description: "<ul><li>1 Coaching Session</li><li>Attendance for up to 3 private seminars included</li><li>LSAT Cracked Book</li></ul>",
+  price: 700,
+  full_price: 925,
+)
+
+Product.create!(
+  name: "Package 2",
+  sku: "package-2",
+  description: "<ul><li>3 Coaching Sessions</li><li>Attendance for up to 10 private seminars included</li><li>LSAT Cracked Book</li></ul>",
+  price: 2400,
+  full_price: 3300,
+)
+
+Product.create!(
+  name: "Package 3",
+  sku: "package-3",
+  description: "<ul><li>10 Coaching Sessions</li><li>Attendance for up to 20 private seminars included</li><li>LSAT Cracked Book</li><li>Customized study plan</li></ul>",
+  price: 6200,
+  full_price: 8550,
+)
