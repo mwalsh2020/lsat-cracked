afdc524e1540168b69068df17315db1f7ee9b33f
Caio Andrade <1820441+caioertai@users.noreply.github.com>
2020-09-18 21:40:22 -0500

    Add questions to section

--- app/models/section.rb
+++ app/models/section.rb
@@ -4,6 +4,8 @@ class Section < ApplicationRecord
   belongs_to :chapter
 
   has_many :quizzes, as: :quizable, dependent: :destroy
+  has_many :quizable_questions, as: :quizable, dependent: :destroy
+  has_many :questions, through: :quizable_questions
 
   acts_as_list scope: :chapter
 end
