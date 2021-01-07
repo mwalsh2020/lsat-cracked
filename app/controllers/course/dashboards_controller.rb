289d4a7f8ff010ee4edb2e94185937d846a129a6
Jamieson Reinhard <jamieson.reinhard2@gmail.com>
2021-01-07 10:28:49 -0700

    removed comment as things are no longer insane

--- app/controllers/course/dashboards_controller.rb
+++ app/controllers/course/dashboards_controller.rb
@@ -1,9 +1,7 @@
 class Course::DashboardsController < Course::ApplicationController
   def show
     authorize [:course, :dashboard]
-
-    # TODO: make this not insane!
+    
     @section = policy_scope(Chapter).first.sections.first
-
   end
 end
