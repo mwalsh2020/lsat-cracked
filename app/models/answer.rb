4a5b98d534238111f4ab75138c3ee7ec1b5a32d0
Jamieson Reinhard <59634117+jamiesonreinhard@users.noreply.github.com>
2021-01-26 08:42:04 -0700

    138 - Listing wrongly answered questions from the last three days (#146)
    * now listing wrongly answered questions from prior three days in the dashboard. this needs further styling
    
    * changed header text for dashboard
    
    * removed card and simplified dashboard with an ordered list of the missed questions over the past three days
    
    * update
    
    * styled the wrong answers list in the dashboard

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -4,6 +4,10 @@ class Answer < ApplicationRecord
   has_rich_text :content
   has_many :quiz_questions, dependent: :destroy
 
+  scope :correct, -> { where(correct: true) }
+  scope :wrong, -> { where(correct: false) }
+  scope :recently_answered, -> { where("quiz_questions.updated_at > ?", 3.days.ago )}
+
   def name
     content
   end
