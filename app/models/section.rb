85f51e96b1034b80a5ac219759acb6b33b2a0810
Caio Andrade <andrade@hey.com>
2021-02-01 16:37:27 -0600

    Add quizable module to article

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,11 +1,9 @@
 class Section < ApplicationRecord
+  include Quizable
+
   has_rich_text :content
 
   belongs_to :chapter
-
-  has_many :quizzes, as: :quizable, dependent: :destroy
-  has_many :quizable_questions, as: :quizable, dependent: :destroy
-  has_many :questions, through: :quizable_questions
 
   acts_as_list scope: :chapter
 end
