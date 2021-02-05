1c6db41db9ab1655d03090b74df842aaab434ce9
Caio Andrade <andrade@hey.com>
2021-02-05 16:28:37 -0600

    Add authentication service for Lawhub

--- app/services/lawhub/authentication.rb
+++ app/services/lawhub/authentication.rb
@@ -1,21 +1,26 @@
 class Lawhub::Authentication
-  def access_token
-    consumer_key = "<consumer_key>"
-    consumer_secret = "<consumer_secret>"
+  attr_reader :credentials, :url, :body, :headers
 
-    credentials = Base64.encode64("#{consumer_key}:#{consumer_secret}").gsub("\n", '')
-    url = "https://api.twitter.com/oauth2/token"
-    body = "grant_type=client_credentials"
-    headers = {
+  def initialize
+    @credentials = Lawhub.credentials
+    @url = "https://login.microsoftonline.com/#{Lawhub.config.tenant_id}/oauth2/v2.0/token"
+    @scope = "https://lawpathb2b.onmicrosoft.com/06b7d783-8a46-43de-a375-07059967cb3b/.default"
+    @body = {
+      grant_type: "client_credentials",
+      scope: @scope
+    }
+    @headers = {
       "Authorization" => "Basic #{credentials}",
       "Content-Type" => "application/x-www-form-urlencoded;charset=UTF-8"
     }
-    r = HTTParty.post(url, body: body, headers: headers)
-    bearer_token = JSON.parse(r.body)['access_token']
-    puts "Twitter bearer token is: #{bearer_token}"
+  end
 
-    api_auth_header = {"Authorization" => "Bearer #{bearer_token}"}
-    url = "https://api.twitter.com/1.1/search/tweets.json?q=nba"
-    puts HTTParty.get(url, headers: api_auth_header).body
+  def self.access_token
+    self.new.access_token
+  end
+
+  def access_token
+    r = HTTParty.post(url, body: body, headers: headers)
+    {response: r, token: JSON.parse(r.body)['access_token']}
   end
 end
