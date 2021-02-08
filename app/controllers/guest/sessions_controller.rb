c3acc42f4ae82effbbb5b025fb745bb76502a971
Jamieson Reinhard <jamieson.reinhard2@gmail.com>
2021-02-08 13:01:43 -0700

    add guest controller for instantiating the intro quiz

---
+++ app/controllers/guest/sessions_controller.rb
@@ -0,0 +1,17 @@
+class Guest::SessionsController < ApplicationController
+  def create
+    @quizable = Article.last
+    @quiz = QuizBuilder.new(user: current_user, quizable: @quizable).build
+    @quiz_session = Quiz::Session.new(quiz_session_params.merge(quiz: @quiz))
+    
+    if @quiz_session.save
+      redirect_to root_path(anchor: "intro-quiz")
+    end
+  end
+
+  private
+
+  def quiz_session_params
+    params.require(:quiz_session).permit(quiz_questions_attributes: [:id, :answer_id])
+  end
+end
