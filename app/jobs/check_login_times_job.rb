d08eeec5d4cafc0106958de7eb69a50faed30f32
Jamieson Reinhard <jamieson.reinhard2@gmail.com>
2021-02-09 13:40:17 -0700

    replaced User.all.each with User.find_each.select

--- app/jobs/check_login_times_job.rb
+++ app/jobs/check_login_times_job.rb
@@ -2,20 +2,21 @@ class CheckLoginTimesJob < ApplicationJob
   queue_as :default
 
   def perform
-    logged_in = []
     p "We're checking the users to see when they last logged in."
-    User.all.each do |user|
-      sign_in_date = user.last_sign_in_at ? user.last_sign_in_at.to_date : nil
-      week_ago = 7.days.ago.to_date
-      if sign_in_date && sign_in_date < week_ago
-        p "#{user.email} logged in this week."
-        login(user)
-      end
+    recent_users = User.find_each.select do |user|
+      login_this_week?(user)
     end
-    p "We pinged LSAC for these users: #{logged_in}"
+    recent_users.each do |user|
+      p "#{user.email} logged in this week."
+      ping_lsac(user)
+    end
   end
 
-  def login(user)
+  def login_this_week?(user)
+    user.last_sign_in_at && user.last_sign_in_at.to_date < 7.days.ago.to_date
+  end
+
+  def ping_lsac(user)
     service = Lawhub::User.new(user)
     service.login
   end
