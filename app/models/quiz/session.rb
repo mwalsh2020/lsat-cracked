07cb26021e953a5e5da2e72e6d34cbcbeb3fb3bf
Caio Andrade <andrade@hey.com>
2021-02-09 13:13:25 -0600

    Update Session to work with unpersisted quizes

--- app/models/quiz/session.rb
+++ app/models/quiz/session.rb
@@ -2,23 +2,33 @@
 class Quiz::Session
   include ActiveModel::Model
 
-  attr_accessor :quiz
+  attr_writer :quiz
   attr_reader :quiz_questions
 
   delegate :user, :complete?, to: :quiz
 
   def initialize(params)
     @quiz_questions = params[:quiz].quiz_questions
+    @quiz = params[:quiz]
     super(params)
     model_name.instance_eval { @route_key = "sessions" }
   end
 
+  def quiz
+    @quiz ||= Quiz.new
+  end
+
   def quiz_questions_attributes=(values)
-    @quiz_questions = values.map do |_key, quiz_question_params|
-      quiz_question = QuizQuestion.find(quiz_question_params[:id])
+    quiz_questions_proxy = values.map do |_key, quiz_question_params|
+      quiz_question = QuizQuestion.find_by(id: quiz_question_params[:id]) ||
+                      QuizQuestion.new(quiz_question_params)
       quiz_question.assign_attributes(quiz_question_params)
+      quiz_question.question ||= quiz_question.answer.question
+      quiz_question.quiz ||= quiz
       quiz_question
     end
+    quiz.quiz_questions = quiz_questions_proxy
+    @quiz_questions = quiz.quiz_questions
   end
 
   def valid?
@@ -30,11 +40,11 @@ class Quiz::Session
   end
 
   def correct_answers_ratio
-    correct_answers_count.fdiv(@quiz_questions.count)
+    correct_answers_count.fdiv(@quiz_questions.size)
   end
 
   def correct_answers_count
-    @quiz_questions.correctly_answered.count
+    @quiz_questions.count(&:correct?)
   end
 
   def good_correct_ratio?
