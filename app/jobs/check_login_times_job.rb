1237549502c04e020aa4bb059d9249ab34743f31
Jamieson Reinhard <jamieson.reinhard2@gmail.com>
2021-02-09 12:37:48 -0700

    created job to check which users logged in during last 7 days and log them into LSAC

---
+++ app/jobs/check_login_times_job.rb
@@ -0,0 +1,20 @@
+class CheckLoginTimesJob < ApplicationJob
+  queue_as :default
+
+  def perform
+    logged_in = []
+    p "We're checking the users to see when they last logged in."
+    User.all.each do |user|
+      sign_in_date = user.last_sign_in_at ? user.last_sign_in_at.to_date : nil
+      week_ago = 7.days.ago.to_date
+      if sign_in_date && sign_in_date < week_ago
+        logged_in << user.email
+        p "#{user.email} logged in this week."
+        service = Lawhub::User.new(user)
+        service.login
+        return user
+      end
+    end
+    p "We pinged LSAC for these users: #{logged_in}"
+  end
+end
