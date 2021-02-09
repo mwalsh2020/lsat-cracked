967ae45168bfc6e76dc36b85bb6a337246fcf3d1
Caio Andrade <andrade@hey.com>
2021-02-09 11:18:53 -0600

    Skip auth on guest sesssion controller

--- app/controllers/guest/sessions_controller.rb
+++ app/controllers/guest/sessions_controller.rb
@@ -1,9 +1,11 @@
 class Guest::SessionsController < ApplicationController
+  skip_before_action :authenticate_user!
+
   def create
     @quizable = Article.last
     @quiz = QuizBuilder.new(user: current_user, quizable: @quizable).build
     @quiz_session = Quiz::Session.new(quiz_session_params.merge(quiz: @quiz))
-    
+
     if @quiz_session.save
       redirect_to root_path(anchor: "intro-quiz")
     end
