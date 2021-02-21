e3db9c02ae584d621855747e5a77b591d39d2fc5
Caio Andrade <andrade@hey.com>
2021-02-21 16:39:49 -0600

    Update ping lsac job for better use of instance variables and better public interface

--- app/jobs/ping_lsac_job.rb
+++ app/jobs/ping_lsac_job.rb
@@ -2,13 +2,15 @@ class PingLsacJob < ApplicationJob
   queue_as :default
 
   def perform(user_id)
-    user = User.find(user_id)
-    puts "#{user.email} logged in this week."
-    ping_lsac(user)
+    @user = User.find(user_id)
+    puts "#{@user.email} logged in this week."
+    ping_lsac
   end
 
-  def ping_lsac(user)
-    service = Lawhub::User.new(user)
+  private
+
+  def ping_lsac
+    service = Lawhub::User.new(@user)
     service.login
   end
 end
