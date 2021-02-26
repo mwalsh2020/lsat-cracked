935436a3ea9d6261236861acd37d8ea1224e3405
Caio Andrade <andrade@hey.com>
2021-02-25 23:02:10 -0600

    Refactor authorization position to outside the render

--- app/javascript/controllers/intro_quiz_controller.js
+++ app/javascript/controllers/intro_quiz_controller.js
@@ -1,7 +1,10 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  connect() {
-    console.log('Hello, Stimulus!')
+  static targets = ["form"]
+
+  showQuiz() {
+    debugger
+    console.log(this.formTarget)
   }
 }
