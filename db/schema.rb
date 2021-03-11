330c2a8674c48670ec51f0c90c23b227de740266
Jamieson Reinhard <jamieson.reinhard2@gmail.com>
2021-03-11 13:07:22 -0700

    added intro boolean to articles model, default false

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_26_200202) do
+ActiveRecord::Schema.define(version: 2021_03_11_200258) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 2021_01_26_200202) do
     t.boolean "published", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "intro", default: false
   end
 
   create_table "chapters", force: :cascade do |t|
