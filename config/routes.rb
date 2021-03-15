7e4a0d985053ec88b6b131bf88c4dbdffd65f053
Caio Andrade <andrade@hey.com>
2021-03-14 20:50:48 -0600

    Move routes to admin route authentication block

--- config/routes.rb
+++ config/routes.rb
@@ -6,22 +6,23 @@ Rails.application.routes.draw do  devise_for :users
   require "sidekiq/web"
   authenticate :user, ->(user) { user.admin? } do
     mount Sidekiq::Web => '/sidekiq'
-  end
 
-  namespace :admin do
-    resources :answers, except: %i[new create]
-    resources :articles
-    resources :chapters
-    resources :products
-    resources :questions do
-      resources :answers, only: %i[new create]
+    namespace :admin do
+      resources :answers, except: %i[new create]
+      resources :articles
+      resources :chapters
+      resources :products
+      resources :questions do
+        resources :answers, only: %i[new create]
+      end
+      resources :sections
+      resources :tags
+      resources :users
+
+      root to: "chapters#index"
     end
-    resources :sections
-    resources :tags
-    resources :users
-
-    root to: "chapters#index"
   end
+
 
   resources :coaching_requests, only: [:new, :create]
   resources :mail_leads, only: [:create]
