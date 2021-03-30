9985f9d8e39347c74d5691da53d121bc700228a6
Caio Andrade <andrade@hey.com>
2021-03-30 16:34:38 -0600

    Add initialize specs for QuestionTagData

--- app/models/query/questions_tag_data.rb
+++ app/models/query/questions_tag_data.rb
@@ -1,3 +1,7 @@
 class Query::QuestionsTagData
+  def initialize(questions)
+    raise ArgumentError, "an ActiveRecord Relation must be passed" unless questions.is_a?(ActiveRecord::Relation)
 
+    @questions = questions
+  end
 end
--- spec/models/query/questions_tag_data_spec.rb
+++ spec/models/query/questions_tag_data_spec.rb
@@ -2,9 +2,11 @@ require "rails_helper"
 
 RSpec.describe Query::QuestionsTagData do
   describe ".new" do
-    it "accepts a question relation" do
+    it "raises error unless given a question relation" do
       questions = Question.all
 
+      expect { described_class.new }.to raise_error(ArgumentError)
+      expect { described_class.new([]) }.to raise_error(ArgumentError)
       expect { described_class.new(questions) }.to_not raise_error
     end
   end
