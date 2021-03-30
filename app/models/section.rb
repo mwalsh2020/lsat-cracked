393a423c98c64b9e82f0bf8867bc21e4464d2fbe
Caio Andrade <andrade@hey.com>
2021-03-30 14:34:09 -0600

    Add Section#previous and Section#next

--- app/models/section.rb
+++ app/models/section.rb
@@ -6,4 +6,12 @@ class Section < ApplicationRecord
   belongs_to :chapter
 
   acts_as_list scope: :chapter
+
+  def previous
+    higher_item || chapter.higher_item&.sections&.last
+  end
+
+  def next
+    lower_item || chapter.lower_item&.sections&.first
+  end
 end
