ea6cb2dd2c8515acda433e40230c1d59e643ddb9
Caio Andrade <andrade@hey.com>
2021-04-08 14:49:45 -0500

    Add cached QuoteFetcher.quote_of_the_day

--- app/services/quote_fetcher.rb
+++ app/services/quote_fetcher.rb
@@ -1,18 +1,32 @@
-require 'httparty'
-require 'json'
-
 class QuoteFetcher
   include Executable
 
-  def initialize; end
+  class Quote < OpenStruct
+    def author
+      super || "Anonymous"
+    end
+  end
+
+  def self.quote_of_the_day
+    Rails.cache.fetch(:cached_quote, expires_in: 24.hours) do
+      Quote.new(execute.sample)
+    end
+  end
 
   def execute
-    response = JSON.parse(fetch_quotes)
+    JSON.parse(fetch_quotes)
   end
 
   private
 
   def fetch_quotes
-    response = HTTParty.get("https://type.fit/api/quotes")
+    HTTParty.get("https://type.fit/api/quotes")
+  end
+
+  def default_quote
+    Quote.new(
+      text: "The truest wisdom is a resolute determination.",
+      author: "Napoleon Bonaparte"
+    )
   end
 end
