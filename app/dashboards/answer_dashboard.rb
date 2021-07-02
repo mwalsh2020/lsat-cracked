e1855a51fc9d87af85bb45fa72ea65d453516851
Enrique Ramirez <nrqrmz@gmail.com>
2021-07-02 10:49:04 -0500

    Add question field for the answers dashboard

--- app/dashboards/answer_dashboard.rb
+++ app/dashboards/answer_dashboard.rb
@@ -20,8 +20,9 @@ class AnswerDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = %i[
-    content
     correct
+    content
+    question
   ].freeze
 
   # SHOW_PAGE_ATTRIBUTES
