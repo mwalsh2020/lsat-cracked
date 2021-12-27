a6bbf3eab91f88015d62a14eeb001149e7a5269b
Daniel Polo <od.polo@gmail.com>
2021-12-27 11:28:17 -0600

    fix: change root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root                  to: "pages#home"
+  root                  to: "course/dashboards#show"
   get  "/about",        to: "pages#about"
   get  "/testimonials", to: "pages#testimonials"
   get "/guest/sessions", to: "pages#home"
